093516ff4e0505b83aa82fc74acf5294109b3cc9
lesliesage <leslie.sage@gmail.com>
2019-08-28 09:55:22 -0400

    Done.

--- src/MasterHog.js
+++ src/MasterHog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Master from './assets/master-hog.png'
 import BabyHog from './BabyHog'
-// import offspring from './db.js'
+import offspring from './db.js'
 
 export default class MasterHog extends Component {
 
@@ -19,6 +19,13 @@ export default class MasterHog extends Component {
     })
   }
 
+  piglet = () => offspring.map(piglet => {return <BabyHog 
+    key={piglet.id}
+    name={piglet.name}
+    hobby={piglet.hobby}
+    eyeColor={this.state.eyeColor}
+    />
+  })
 
   render() {
     return (
@@ -40,9 +47,7 @@ export default class MasterHog extends Component {
         </div>
         
         <ul className="hoglist">
-          <BabyHog />
-          <BabyHog />
-          <BabyHog />
+          {this.piglet()}
         </ul>
 
       </div>
